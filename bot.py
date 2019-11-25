8b5d4cbc6dc01855b5aedb53221139295c0dcaf7
Wagner Braga dos Santos <wag.097@gmail.com>
2019-11-25 16:40:30 -0300

    crop

--- bot.py
+++ bot.py
@@ -1,17 +1,32 @@
 import timeit
-code_to_test = """
 import pyautogui
-import PIL
+
 from pytesseract import image_to_string
 
+def foo():
+    return screenshot
 
-print(image_to_string(PIL.Image.open('wagner.png')))
+code_to_test = """
+import PIL
+import pyautogui
+from pytesseract import image_to_string
+screenshot = pyautogui.screenshot("file.png")
+area1 = (40, 10,90, 25)
+part1 = screenshot.crop(area1)
+
+print(image_to_string(part1))
+
+
+print(screenshot)
+area2 = (500, 500,550, 550)
+
+part2 = screenshot.crop(area2)
+part2.save("part2.png")
+print(image_to_string(part2))
 """
-#pyautogui.keyDown("y")
-
-#pyautogui.keyUp("y")
-
-#pyautogui.moveTo(3500,200,duration=0.2)
-
 elapsed_time = timeit.timeit(code_to_test, number=1)/1
 print(elapsed_time)
+
+
+#screenshot = pyautogui.screenshot("file.png", region =(40, 10,50, 15) )
+#screenshot.show()
