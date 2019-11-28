abc382d75e31e57e955099c00d3f99addec1578b
Wagner Braga dos Santos <wag.097@gmail.com>
2019-11-28 15:38:45 -0300

    p

--- p2/q1.py
+++ p2/q1.py
@@ -18,37 +18,49 @@ v4_arange = numpy.arange(0, 100, 0.0001)
 def f_q1(x, b):
     return pow(x,3) + pow(x,2) + b
 
-def dif_frente(v, b, f):
-    lista = []
-    h = v[1] - v[0]
-    for x in range(0,v.size-1):
-        lista.append((f(x + h, b) - f(x, b))/h)
-    lista.append(0)
-    return numpy.array(lista)
-
-def dif_tras(v, b, f):
+def d2_x(v, b, f):
     lista = [0]
     h = v[1] - v[0]
-    for x in range(1,v.size):
+    for x in v[1:-1]:
+        lista.append((f(x+h, b) - 2*f(x,b) + f(x-h, b))/pow(h,2))
+    lista.append(0)
+    return numpy.array(lista, dtype=object)
+
+def d1_frente_x(v, b, f):
+    lista = []
+    h = v[1] - v[0]
+    for x in v[:-1]:
+        lista.append((f(x + h, b) - f(x, b))/h)
+    lista.append(0)
+    print(lista)
+    return numpy.array(lista)
+
+def d1_tras_x(v, b, f):
+    lista = [0]
+    h = v[1] - v[0]
+    for x in v[1:]:
         lista.append((f(x, b) - f(x - h, b))/h)
     return numpy.array(lista)
 
-def dif_central(v, b, f):
+def d1_central_x(v, b, f):
     lista = [0]
     h = v[1] - v[0]
-    for x in range(1,v.size-1):
+    for x in v[1:-1]:
         lista.append((f(x + h, b) - f(x - h, b))/(2*h))
     lista.append(0)
     return numpy.array(lista)
 
-def derivada(x):
+def d1(x):
     return 3*pow(x,2) + 2 * x
+
+def d2(x):
+    return 6*x + 2
 
 def dif_normalzera(v, b, f):
     lista = []
-    for x in range(v.size):
+    for x in v:
         lista.append(f(x))
     return numpy.array(lista)
 
-print(dif_central(v1_linspace, b, f_q1)[0:10])
-print(dif_normalzera(v1_linspace, b,  derivada)[0:10])
+print(d2_x(v3_arange, b, f_q1)[0:10])
+print(dif_normalzera(v3_arange, b,  d2)[0:10])
